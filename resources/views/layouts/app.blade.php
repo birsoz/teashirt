6021a5286f86e6c149729dd4da8d941d6e230d02
Dusty <dusty@teashirt.tea>
2020-04-09 10:37:50 +0200

    small fixes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,8 +13,7 @@
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
         <style>
             html, body {
-                padding-top: 75px;
-                padding: 65 px;
+                padding: 65px;
                 background-color: #fff;
                 color: #636b6f;
                 font-family: 'Nunito', sans-serif;
