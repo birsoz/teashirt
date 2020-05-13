5e5febec6b2b684daba91f70e3e399aee8df7bca
Dusty <dusty@teashirt.tea>
2020-05-13 16:12:39 +0200

    sale-stock-prices-categories-tags-is_active columns are added to database

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -91,7 +91,7 @@
             @endguest
         </ul>  
         <form class="form-inline my-2 my-lg-0">
-            <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
+            <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search" action="ProductsController@index">
             <button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
         </form>
     </div>
