bac46224b5af18add00d2cbf4b5c0705450e7b02
Dusty <dusty@teashirt.tea>
2020-05-13 16:45:30 +0200

    attributes added to create and edit pages(needs handling next)

--- resources/views/pages/create.blade.php
+++ resources/views/pages/create.blade.php
@@ -11,9 +11,35 @@
         {{Form::label('description' , 'Description')}}
         {{Form::textarea('description','',['class' => 'form-control', 'placeholder' => 'Description'])}}
     </div>
+        
+    <div class="form-group">
+        {{Form::label('is_active' , 'Is Active?')}}
+        {{Form::checkbox('is_active', 'value', true)}}
+    </div>
+    <div class="form-group">
+        {{Form::label('in_sale' , 'In Sale?')}}
+        {{Form::checkbox('in_sale', 'value', false)}}
+    <div class="form-group">
+        {{Form::label('base_price' , 'Base Price')}}
+        {{Form::number('base_price', 'value')}}
+        {{Form::label('sale_price' , 'Sale Price')}}
+        {{Form::number('sale_price', 'value')}}
+    </div>
+    <div class="form-group">
+        {{Form::label('categories' , 'Categories')}}
+        {{Form::select('categories', array(
+        'Select a category please',
+        'Women' => array('tshirts' => 'T-Shirts', 'jumpers'=> 'Jumpers', 'jackets'=> 'Jackets'),
+        'Men' => array('tshirts' => 'T-Shirts', 'jumpers'=> 'Jumpers', 'jackets'=> 'Jackets'),
+        'Children' => array('tshirts' => 'T-Shirts', 'jumpers'=> 'Jumpers', 'jackets'=> 'Jackets'),
+        'Accessories' => array('totebags' => 'Tote bags', 'bandanas'=> 'Bandanas', 'hats'=> 'Hats'),
+    ))}}
+    </div>
     {{Form::file('Image_Source')}}
     {{Form::submit('Submit', ['class' => 'btn btn-primary'])}}
     {!! Form::close() !!}
+
+
     {{-- POST method is not supported error comes out from the following snippet --}}
         
     {{-- <form action="'ProductsController@store'" method="POST">
--- resources/views/pages/edit.blade.php
+++ resources/views/pages/edit.blade.php
@@ -11,6 +11,29 @@
         {{Form::label('description' , 'Description')}}
         {{Form::textarea('description', $product->Description, ['class' => 'form-control', 'placeholder' => 'Description'])}}
     </div>
+    <div class="form-group">
+        {{Form::label('is_active' , 'Is Active?')}}
+        {{Form::checkbox('is_active', 'value', true)}}
+    </div>
+    <div class="form-group">
+        {{Form::label('in_sale' , 'In Sale?')}}
+        {{Form::checkbox('in_sale', 'value', false)}}
+    <div class="form-group">
+        {{Form::label('base_price' , 'Base Price')}}
+        {{Form::number('base_price', 'value')}}
+        {{Form::label('sale_price' , 'Sale Price')}}
+        {{Form::number('sale_price', 'value')}}
+    </div>
+    <div class="form-group">
+        {{Form::label('categories' , 'Categories')}}
+        {{Form::select('categories', array(
+        'Select a category please',
+        'Women' => array('tshirts' => 'T-Shirts', 'jumpers'=> 'Jumpers', 'jackets'=> 'Jackets'),
+        'Men' => array('tshirts' => 'T-Shirts', 'jumpers'=> 'Jumpers', 'jackets'=> 'Jackets'),
+        'Children' => array('tshirts' => 'T-Shirts', 'jumpers'=> 'Jumpers', 'jackets'=> 'Jackets'),
+        'Accessories' => array('totebags' => 'Tote bags', 'bandanas'=> 'Bandanas', 'hats'=> 'Hats'),
+    ))}}
+    </div>
     {{Form::file('Image_Source')}}
     {{Form::submit('Submit', ['class' => 'btn btn-primary'])}}
     {{Form::hidden('_method' , 'PUT')}}
