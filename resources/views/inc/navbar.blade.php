d4225c97a45798aeb9b42c5f719645395208ae64
Dusty <dusty@teashirt.tea>
2020-05-17 19:42:41 +0200

    instead of url search action is now controlled by Pro.Con.@index

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -90,7 +90,9 @@
                 </li>
             @endguest
         </ul>  
-        <form action= "/teashirt/public" class="form-inline my-2 my-lg-0">
+        {{-- Both works! --}}
+        {{-- <form action= "/teashirt/public" class="form-inline my-2 my-lg-0"> --}}
+        <form action= "{{action('ProductsController@index')}}" class="form-inline my-2 my-lg-0">
             <input class="form-control mr-sm-2" type="text" name="search" placeholder="Search">
             <button class="btn btn-secondary my-2 my-sm-0" type="submit" >Search</button>
         </form>
